72040dc558f0371a1053a7a6b2234c4694fb11b0
celikbas <celikbas@itu.edu.tr>
2013-11-06 09:50:45 +0200

    check_filename_from_csv.py gelistirildi

--- check_filename_from_csv.py
+++ check_filename_from_csv.py
@@ -10,19 +10,43 @@ __status__      = "Experiment"
 
 import csv
 import os
+import glob
 
-reader = csv.DictReader(open('31_Ekim13CDM.csv', 'rb'), delimiter='\t')
+reader = csv.DictReader(open('31_Ekim13CDM.txt', 'rb'), delimiter='\t')
 
+dir_log = open('dir_log.txt','w')
+#file_log = open('file_log.txt','w')
 for row in reader:
-	DIR = row.get('FileName')
-	PDF = DIR + '.pdf'
-	if not os.path.exists(DIR):
-		os.mkdir(DIR)
-		print "Folder created: " + DIR
+    DIR = row.get('FileName')
+    PDF = DIR + '.pdf'
 
-	if os.path.exists(PDF):
-		COMMD = 'pdfimages -j ' + PDF + ' ' + DIR + '/' + DIR
-		os.system(COMMD)
-		print "File processed to folder " + DIR + " Command: " + COMMD
+    if os.path.exists(PDF):
+        if os.path.exists(DIR):
+            files = glob.glob(DIR+'/*')
+            for f in files:
+                os.remove(f)
 
+            message = "DIRECTORY EXIST. Old files removed: " + DIR + '\n'
+            dir_log.write(message)
+        else:
+            os.mkdir(DIR)
+            message = 'Folder created ' + DIR + '\n'
+            dir_log.write(message)
+            # print message
+    
+        COMMD = 'pdfimages -j ' + PDF + ' ' + DIR + '/' + DIR
+        os.system(COMMD)
+        message = "File processed to folder: " + DIR + '\n'
+        dir_log.write(message)
+        files = glob.glob(DIR+'/*')
+        for f in files:
+            dir_log.write(' ' + f + '\n')
+        # print message
+    else:
+        message = "ERROR: File not exist: " + PDF + '\n'
+        dir_log.write(message)
+        #print message
+
+dir_log.close()
+# file_log.close()
 
